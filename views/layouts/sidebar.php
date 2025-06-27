b6f0e14fa7de7bc31b971f64c56dcb97fad28c9b
juniel <juniel.programador@gmail.com>
2025-06-27 09:08:26 -0400

    ajustar diseño sispsa

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -2,21 +2,20 @@
 use yii\bootstrap4\Nav;
 use mdm\admin\components\MenuHelper;
 ?>
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="border-bottom: none; background-color: #007bff !important; color: white !important;">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="index3.html" class="brand-link d-flex justify-content-center" style="padding-top: 0; padding-bottom: 0;">
+        <img src="<?= Yii::getAlias('@web') ?>/img/sispsa-12-62.png" alt="Logo" class="brand-image" style="opacity: .8; margin-top: 20px; margin-bottom: 10px; margin-left: auto; margin-right: auto; max-width: 350px; max-height: 450px; width: auto; height: auto; object-fit: contain;">
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex flex-column align-items-center">
+            <div class="image mb-2">
+                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" style="width: 100px; height: 100px;" alt="User Image">
             </div>
-            <div class="info">
+            <div class="info text-center text-uppercase">
                 <a href="#" class="d-block">Alexander Pierce</a>
             </div>
         </div>
