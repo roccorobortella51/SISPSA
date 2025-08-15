a010a74f8b5c486a117cd768cf572309d53d7b1e
marcosipod <marcosipod@gmail.com>
2025-08-14 22:44:56 -0400

    ajustar volver

--- views/declaracion-de-salud/update.php
+++ views/declaracion-de-salud/update.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = 'ACTUALIZAR';
 
 <div class="col-md-12 text-end">
     <div class="float-right" style="margin-bottom:10px;">
-        <?= Html::a('<i class="fas fa-undo"></i> Volver', ['index'], ['class' => 'btn btn-info btn-lg']) ?> 
+        <?= Html::a('<i class="fas fa-undo"></i> Volver', ['index', 'user_id' => $model->user_id], ['class' => 'btn btn-info btn-lg']) ?> 
     </div>
 </div>
 
