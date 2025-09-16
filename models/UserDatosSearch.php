7c53c5d69ea66bfa1b2d8d8980444a516cf6ef1e
marcosipod <marcosipod@gmail.com>
2025-09-16 19:27:26 -0400

    actualizar cpanel cdula 10

--- models/UserDatosSearch.php
+++ models/UserDatosSearch.php
@@ -162,7 +162,7 @@ class UserDatosSearch extends UserDatos
             ->andFilterWhere(['ilike', 'user_datos.tipo_cedula', $this->tipo_cedula])
             ->andFilterWhere(['ilike', 'user_datos.tipo_sangre', $this->tipo_sangre])
             ->andFilterWhere(['ilike', 'user_datos.estatus_solvente', $this->estatus_solvente])
-            ->andFilterWhere(['ilike', 'user_datos.cedula', $this->estatus_solvente])
+            ->andFilterWhere(['ilike', 'user_datos.cedula', $this->cedula])
             ->andWhere(['is', 'user_datos.deleted_at', null]);
 
         return $dataProvider;
